db6ae360577b7c7c1555564a051d3284d08001b3
liqg5 <liqg5@ziroom.com>
2018-03-26 16:14:59 +0800

    添加项目

---
+++ podsDemo/podsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podsDemo
+//
+//  Created by liqg5 on 2018/3/26.
+//  Copyright © 2018年 liqg5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podsDemo/podsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podsDemo
+//
+//  Created by liqg5 on 2018/3/26.
+//  Copyright © 2018年 liqg5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
