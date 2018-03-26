415b18c12069324b185340933b53d880dca5e993
liqg5 <liqg5@ziroom.com>
2018-03-26 17:24:42 +0800

    添加公开的文件

---
+++ podsDemo/podsDemo/Classes/ZRDeviceUtils.h
@@ -0,0 +1,20 @@
+//
+//  ZRDeviceUtils.h
+//  ZRUtil
+//
+//  Created by liqg5 on 2018/3/22.
+//  Copyright © 2018年 ziroomer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZRDeviceUtils : NSObject
+
+/*!
+ @method
+ @abstract 打电话
+ @param phoneStr 需要转换的图片
+ */
++ (void)zr_openUrl:(NSString *)phoneStr;
+
+@end
---
+++ podsDemo/podsDemo/Classes/ZRDeviceUtils.m
@@ -0,0 +1,28 @@
+//
+//  ZRDeviceUtils.m
+//  ZRUtil
+//
+//  Created by liqg5 on 2018/3/22.
+//  Copyright © 2018年 ziroomer. All rights reserved.
+//
+
+#import "ZRDeviceUtils.h"
+#import <UIKit/UIKit.h>
+#import "ZRColorConstant.h"
+
+@implementation ZRDeviceUtils
+
++ (void)zr_openUrl:(NSString *)phoneStr{
+    NSString *tel = [[NSMutableString alloc] initWithFormat:@"telprompt://%@",phoneStr];
+    //
+    if (@available(iOS 10.0, *)) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:tel] options:@{} completionHandler:^(BOOL success) {
+            
+        }];
+    } else {
+        // Fallback on earlier versions
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:tel]];
+    }
+}
+
+@end
